a53f8964e9949cfc2dba3942800dd18e7c97d7ee
Caleb Simpson <caleb@simpson.center>
2026-03-19 10:17:47 -0400

    Starting to configure nvim

--- arch/fish/config.fish
+++ arch/fish/config.fish
@@ -1,12 +1,15 @@
 # Source the default cachyOS config for fish, if available
 if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
-  source /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
-# Make sure ssh-agnet is available
+# Make sure ssh-agent is available
 if not set -q SSH_AGENT_PID
-  ssh-agent -c | source > /dev/null 2>&1
+    ssh-agent -c | source >/dev/null 2>&1
 end
 
 # Include ~/.local/bin in the PATH
 set -gx PATH $HOME/.local/bin $PATH
+
+# === Aliases ===
+alias vim nvim
--- arch/nvim/lua/config/options.lua
+++ arch/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.wrap = true -- Wrap text instead of horizontal scroll
+vim.opt.linebreak = true -- wraps at word boundaries (nicer)
+vim.opt.breakindent = true -- keeps indentation on wrapped lines
+
+vim.opt.number = true -- show line numbers
+vim.opt.relativenumber = false -- use absolute line numbers
