bfdd07f282eddd7f396e7f0ec90cc336a7dfde2a
Caleb Simpson <caleb@simpson.center>
2026-05-13 11:37:23 -0400

    Setup scripts for git and dbeaver

---
+++ arch/setup_scripts/git.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+# We assume that git is already installed because how else would you have this dotfiles repo?
+# Here we just configure it a bit.
+
+git config --global core.editor "vim"
